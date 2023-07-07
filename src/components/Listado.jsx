555eb942cc8a99a1a368dcc9a7f1a91b092a8494
DavidXploidz <david.adame.itd@gmail.com>
2023-07-07 16:55:38 -0600

    ajustes por fecha parcial

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,15 +1,29 @@
 import Movimiento from "./Movimiento";
-import useApp from "../hooks/useApp"
+import useApp from "../hooks/useApp";
+import obtenerFecha from '../helpers/obtenerFecha';
 
 export default function Listado() {
 
     const { movimientos } = useApp();
 
+    const movimientos_hoy = movimientos.filter(mov => mov.fecha === obtenerFecha());
+    const movimientos_antes = movimientos.filter(mov => mov.fecha !== obtenerFecha());
+  
   return (
-    <div className="mt-3 p-0 col-lg-6 mx-auto d-flex flex-column-reverse">
-        {movimientos.map(movimiento => (
-            <Movimiento key={movimiento.id} movimiento={movimiento} />
-        ))}
+    <div className="mt-3 p-0 col-lg-6 mx-auto">
+        <p className="fw-bold mb-0">Hoy - { obtenerFecha()}</p>
+        {movimientos_hoy.map(movimiento => { 
+            return(
+                <Movimiento key={movimiento.id} movimiento={movimiento} />
+            )
+        })}
+        <p className="fw-bold mb-0">Anteriores</p>
+        {movimientos_antes.map(movimiento => { 
+            return(
+                <Movimiento key={movimiento.id} movimiento={movimiento} />
+            )
+        })}
+    
     </div>
   )
 }
